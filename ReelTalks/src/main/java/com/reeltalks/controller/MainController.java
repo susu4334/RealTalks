c66f2c910febc3ece7e3920657c35121ae412770
alsdn <alsdn@DESKTOP-R0CKQFD.mshome.net>
2023-04-21 13:35:18 +0900

    MainController
    Main + Stat 모든 데이터 보내는 함수 추가
    맵핑 주소 : main
    
    메인 컨텐츠 API 맵핑 주소 변경
    mainPage => mainContents
    
    박스 오피스 API 맵핑 주소 변경
    mainBoxOffice => boxOffice

--- ReelTalks/src/main/java/com/reeltalks/controller/MainController.java
+++ ReelTalks/src/main/java/com/reeltalks/controller/MainController.java
@@ -11,23 +11,59 @@ import org.springframework.web.bind.annotation.RestController;
 import com.reeltalks.dto.BoxOfficeDTO;
 import com.reeltalks.dto.HotMovieDTO;
 import com.reeltalks.dto.HotPostDTO;
-import com.reeltalks.dto.MainListDTO;
+import com.reeltalks.dto.MainContentsDTO;
+import com.reeltalks.dto.MainDTO;
 import com.reeltalks.service.MainService;
+import com.reeltalks.service.StatService;
 
 @RestController
 public class MainController {
+	
+	// API 한 번 호출로 모든 데이터를 보내거나, 각 API로 데이터를 받아오기 위해서
+	// 모든 데이터 처리는 DAOimp에서 진행하였습니다.
 
 	@Autowired
 	MainService service;
-
-	@GetMapping("/mainPage")
-	public MainListDTO main() {
-
-		MainListDTO dto = new MainListDTO();
-
+	@Autowired
+	StatService sService;
+	
+	// 영화별 게시물 개수, 영화별 댓글 개수, 카테고리별 게시물 + 댓글 개수
+	// 박스 오피스, 떠오르는 영화, 떠오르는 게시물
+	@GetMapping("main")
+	public MainDTO main() {
+		MainDTO dto = new MainDTO();
+		// 일간 각 영화의 게시물 개수
+		dto.setPost(sService.selectPostFIX());
+		// 일간 각 영화의 댓글 개수		
+		dto.setReply(sService.selectReplyFIX());
+		// 월간 카테고리별 게시물 + 댓글  개수		
+		dto.setGenre(sService.selectGenreFIX());
+		
+		// 박스 오피스
 		dto.setBoxOffice(service.selectBoxOffice());
-		dto.setHotMovie(service.selectHotMoive(14));
-		dto.setHotPost(service.selectHotPost(14));
+		// 떠오르는 영화
+		dto.setHotMovie(service.selectHotMoiveFIX());
+		// 떠오르는 게시물
+		dto.setHotPost(service.selectHotPostFIX());
+		
+		System.out.println(dto);
+		
+		return dto;
+	}
+
+	
+	// 박스 오피스, 떠오르는 영화, 떠오르는 게시물 
+	@GetMapping("/mainContents")
+	public MainContentsDTO mainContents() {
+
+		MainContentsDTO dto = new MainContentsDTO();
+
+		// 박스 오피스
+		dto.setBoxOffice(service.selectBoxOffice());
+		// 떠오르는 영화
+		dto.setHotMovie(service.selectHotMoiveFIX());
+		// 떠오르는 게시물
+		dto.setHotPost(service.selectHotPostFIX());
 
 		System.out.println("MAIN API");
 		System.out.println(dto);
@@ -35,7 +71,8 @@ public class MainController {
 		return dto;
 	}
 
-	@GetMapping("/mainBoxOffice")
+	// 박스 오피스
+	@GetMapping("/boxOffice")
 	public List<BoxOfficeDTO> mainBoxOffice() {
 
 		List<BoxOfficeDTO> list = new ArrayList<BoxOfficeDTO>();
@@ -48,6 +85,7 @@ public class MainController {
 		return list;
 	}
 
+	// 떠오르는 영화
 	@GetMapping("/hotMovie/{day}")
 	public List<HotMovieDTO> hotMovie(@PathVariable("day") int day) {
 
@@ -60,6 +98,7 @@ public class MainController {
 
 	}
 	
+	// 떠오르는 게시물
 	@GetMapping("/hotPost/{day}")
 	public List<HotPostDTO> hotPost(@PathVariable("day") int day) {
 		
