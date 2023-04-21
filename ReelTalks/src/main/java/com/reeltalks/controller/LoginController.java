0d71b062e84dcf8b527e95ba0101b9f1fe9f4a4d
Aero 15 <Aero 15@DESKTOP-7JI65JU>
2023-04-21 12:27:41 +0900

    LoginController 에서
    // 회원등록(Post)
    	@PostMapping("/signup")
    
    return 값
    void 로 변경함

--- ReelTalks/src/main/java/com/reeltalks/controller/LoginController.java
+++ ReelTalks/src/main/java/com/reeltalks/controller/LoginController.java
@@ -44,14 +44,12 @@ public class LoginController {
 	
 	// 회원등록(Post)
 	@PostMapping("/signup")
-	public String signup(@RequestParam("user_id") String user_id, String user_name) {
+	public void signup(@RequestParam("user_id") String user_id, String user_name) {
 		
 		UserDTO dto = 
 				new UserDTO(user_id, user_name, LocalDateTime.now(), LocalDateTime.now());
 		System.out.println(dto);
 		service.insert(dto);
-		
-		return "main.jsp";
 	}
 	
 	// 닉네임 중복 검사
